7ba7ae4eead926ea70034bea53015caf07b27e1f
ale <antoine.le@quicksign.com>
2022-06-28 14:33:33 +0200

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,57 @@
 
 const reporter = require('./aio-tests-log-results');
 const aioLogger = require('./aio-tests-logger')
+const date = new Date(Date.now())
 
 function getAIOConfig(config, reportError) {
-    if(Object.keys(config).includes('env') && Object.keys(config.env).includes('aioTests') && !!config.env.aioTests.enableReporting) {
+    aioLogger.logStartEnd(" Determining config to use")
+    if (config.env.enableReporting != undefined) {
+        config.env.aioTests.enableReporting = config.env.enableReporting
+        aioLogger.log("Nouvelle valeur pour enableReporting : " + config.env.enableReporting);
+    }
+    if (config.env.jiraProjectId != undefined) {
+        config.env.aioTests.jiraProjectId = config.env.jiraProjectId
+        aioLogger.log("Nouvelle valeur pour jiraProjectId : " + config.env.jiraProjectId);
+    }
+    if (config.env.createNewCycle != undefined) {
+        config.env.aioTests.cycleDetails.createNewCycle = config.env.createNewCycle
+        aioLogger.log("Nouvelle valeur pour createNewCycle : " + config.env.createNewCycle);
+    }
+    if (config.env.cycleName != undefined) {
+        if (config.env.cycleName.includes('%Date')) {
+            var str = config.env.cycleName
+            str = str.replace('%Date', date.toLocaleDateString("fr"))
+            aioLogger.log("str : " + str);
+            config.env.cycleName = str
+        }
+        config.env.aioTests.cycleDetails.cycleName = config.env.cycleName
+        aioLogger.log("Nouvelle valeur pour cycleName : " + config.env.cycleName);
+    }
+    if (config.env.cycleKey != undefined) {
+        config.env.aioTests.cycleDetails.cycleKey = config.env.cycleKey
+        aioLogger.log("Nouvelle valeur pour cycleKey : " + config.env.cycleKey);
+    }
+    if (config.env.addNewRun != undefined) {
+        config.env.aioTests.addNewRun = config.env.addNewRun
+        aioLogger.log("Nouvelle valeur pour addNewRun : " + config.env.addNewRun);
+    }
+    if (config.env.addNewRun != undefined) {
+        config.env.aioTests.createNewRunForRetries = config.env.createNewRunForRetries
+        aioLogger.log("Nouvelle valeur pour createNewRunForRetries : " + config.env.createNewRunForRetries);
+    }
+    if (config.env.addAttachmentToFailedCases != undefined) {
+        config.env.aioTests.addAttachmentToFailedCases = config.env.addAttachmentToFailedCases
+        aioLogger.log("Nouvelle valeur pour addAttachmentToFailedCases : " + config.env.addAttachmentToFailedCases);
+    }
+
+    if (Object.keys(config).includes('env') && Object.keys(config.env).includes('aioTests') && !!config.env.aioTests.enableReporting) {
         if (!!!config.env.aioTests.jiraProjectId && !!reportError) {
             aioLogger.error("Jira Project Id is mandatory for AIO Tests Reporting.", true);
             return;
         }
         return config.env.aioTests;
     } else {
-        if(!!reportError)
+        if (!!reportError)
             aioLogger.error("AIO Tests reporting is not enabled.  Please set env:{aioTests:{enableReporting:true}}", true);
     }
 }
@@ -18,9 +59,9 @@ function getAIOConfig(config, reportError) {
 const registerAIOTestsPlugin = (on, config) => {
     on('before:run', () => {
         let aioConfig = getAIOConfig(config, true);
-        if(aioConfig) {
+        if (aioConfig) {
             return reporter.getOrCreateCycle(aioConfig).then((data) => {
-                if(aioConfig.cycleDetails.cycleKeyToReportTo) {
+                if (aioConfig.cycleDetails.cycleKeyToReportTo) {
                     aioLogger.log("Reporting results to cycle : " + aioConfig.cycleDetails.cycleKeyToReportTo);
                 } else {
                     aioLogger.error(data);
@@ -31,8 +72,8 @@ const registerAIOTestsPlugin = (on, config) => {
 
     on('after:spec', (spec, results) => {
         let aioConfig = getAIOConfig(config);
-        if(aioConfig && aioConfig.cycleDetails.cycleKeyToReportTo) {
-            return reporter.reportSpecResults( aioConfig, results).then(() => {
+        if (aioConfig && aioConfig.cycleDetails.cycleKeyToReportTo) {
+            return reporter.reportSpecResults(aioConfig, results).then(() => {
                 aioLogger.logStartEnd(" Reporting results completed.")
             })
         }
